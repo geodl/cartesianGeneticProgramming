0ea51d76101dabffeb0b540e1640f044dd0b9980
tim reid <timreid@gmail.com>
2018-08-16 13:12:50 +0200

    convvert to 1xh topology

--- cgp2.py
+++ cgp2.py
@@ -5,7 +5,7 @@ import operator
 import copy
 
 FunctionRecord = collections.namedtuple("FunctionRecord", ["function", "arity"])
-Genome = collections.namedtuple("Genome", ["n", "m", "r", "c", "functionTable", "nodes", "outputs"])
+Genome = collections.namedtuple("Genome", ["n", "m", "h", "functionTable", "nodes", "outputs"])
 Node = collections.namedtuple("Node", ["functionIndex", "connections"])
 
 def makeFunctionTable(functions):
@@ -22,39 +22,31 @@ def largestConnection(i, n, r):
     #calculates the largest node index which may be connected to node i
     return (i-1 // r) * r + n
 
-def makeRandomGenome(n, m, r, c, functionTable):
-    #constructs a random feed forward genome
+def makeRandomGenome(n, m, h, functionTable):
+    #constructs a random feed forward genome with a 1xh topology
     #n: number of inputs
     #m: number of outputs
-    #r: number of nodes per hidden layer
-    #c: number of hidden layers
+    #h: number of nodes in hidden layer
 
-    outputs = [random.randrange(r*c + n)  for i in range(m)]
+    outputs = [random.randrange(h + n)  for i in range(m)]
 
     nodes = []
-    for i in range(c):
-        for j in range(r):
-            #pick a random function index for this node
-            fi = random.randrange(len(functionTable))
-            record = functionTable[fi]
-            arity = record.arity
-            #compute the index of last node in previous layer
-            #so that randrange(upperBound) gives a valid feed forward connection to this node
-            #upperBound = n + r * i
-            upperBound = largestConnection(i, n, r)
-            #print(f"{upperBound} {largestConnection(i, n, r)}")
-            #generate the connections to this node
-            connections = [random.randrange(upperBound) for k in range(arity)]
-            #put the function index at the start of the node to complete it
-            #node.insert(0, fi)
-            node = Node(fi, connections)
-            nodes.append(node)
+    for i in range(h):
+        #pick a random function index for this node
+        fi = random.randrange(len(functionTable))
+        record = functionTable[fi]
+        arity = record.arity
+        #generate the feed forward connections to this node
+        #node i can connect to nodes 0 .. (n + i)
+        connections = [random.randrange(n + i) for k in range(arity)]
+        node = Node(fi, connections)
+        nodes.append(node)
     
-    genome = Genome(n, m, r, c, functionTable, nodes, outputs)
+    genome = Genome(n, m, h, functionTable, nodes, outputs)
     return genome
 
 def printGenome(genome):
-    print(f"n:{genome.n} m:{genome.m} r:{genome.r} c:{genome.c}")
+    print(f"n:{genome.n} m:{genome.m} h:{genome.h}")
     for node in genome.nodes:
         print(f"(f{node.functionIndex} {node.connections}) ", end="")
     print()
@@ -63,20 +55,18 @@ def printGenome(genome):
 def go():
     functions = [operator.add, operator.neg, operator.mul]
     functionTable = makeFunctionTable(functions)
-    genome = makeRandomGenome(3, 4, 2, 4, functionTable)
+    genome = makeRandomGenome(3, 8, 4, functionTable)
     return genome
 
 def evaluateGenome(genome, inputs):
     #calculates the output of the genome for the supplied input
-    #
     def evalNode(i):
-        n = genome.n
         #base case: input nodes simply output their value
-        if i < n:
+        if i < genome.n:
             return inputs[i]
         else:
             #since input nodes are not present in the node list node i is actually at position i - n
-            node = genome.nodes[i - n]
+            node = genome.nodes[i - genome.n]
             #get the function associated with this node
             f = genome.functionTable[node.functionIndex].function
             #calculate the paramaters to f by recursive descent on the connections
@@ -109,33 +99,33 @@ def onePlusLambdaEvolve(initialGenome, fitness, mutate, nLambda, nGenerations):
                 eliteFitness = individual[1]
     return (elite, eliteFitness)
 
-def makeSimpleMultiPointMutation(nodeMutationRate, outputMutationRate):
-    def mutate(genome):
-        functionTable = genome.functionTable
-        newGenome = copy.deepcopy(genome)
-        for i, node in enumerate(newGenome.nodes):
-            #possibly flip f
-            if random.random() <= nodeMutationRate:
-                #mutate the function index
-                newFunctionIndex = random.randrange(len(functionTable))
-                node.functionIndex = newFunctionIndex
-                newArity = functionTable[newFunctionIndex]
-                if len(node.connections) > newArity:
-                    #need to trim some connections
-                    node.connections = node.connections[:newArity]
-                elif len(node.connections) < newArity:
-                    #need to generate some new connections
-                    for j in range(newArity - len(node.connections)):
-                        upperBound = largestConnection(i, newGenome.n, newGenome.r)
-                        x = random.randrange(upperBound)
-                        node.connections.append(x)
-            #possibly flip each connection
-            for connection in node.connections:
-                if random.random() <= nodeMutationRate:
-                    #make random connection
-            #possibly flip each output
-            for output in node.outputs:
-                if random.random() <= outputMutationRate:
-                    #flippy output
-        return newGenome
-    return mutate
+# def makeSimpleMultiPointMutation(nodeMutationRate, outputMutationRate):
+#     def mutate(genome):
+#         functionTable = genome.functionTable
+#         newGenome = copy.deepcopy(genome)
+#         for i, node in enumerate(newGenome.nodes):
+#             #possibly flip f
+#             if random.random() <= nodeMutationRate:
+#                 #mutate the function index
+#                 newFunctionIndex = random.randrange(len(functionTable))
+#                 node.functionIndex = newFunctionIndex
+#                 newArity = functionTable[newFunctionIndex]
+#                 if len(node.connections) > newArity:
+#                     #need to trim some connections
+#                     node.connections = node.connections[:newArity]
+#                 elif len(node.connections) < newArity:
+#                     #need to generate some new connections
+#                     for j in range(newArity - len(node.connections)):
+#                         upperBound = largestConnection(i, newGenome.n, newGenome.r)
+#                         x = random.randrange(upperBound)
+#                         node.connections.append(x)
+#             #possibly flip each connection
+#             for connection in node.connections:
+#                 if random.random() <= nodeMutationRate:
+#                     #make random connection
+#             #possibly flip each output
+#             for output in node.outputs:
+#                 if random.random() <= outputMutationRate:
+#                     #flippy output
+#         return newGenome
+#     return mutate
