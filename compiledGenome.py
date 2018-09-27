591509299b0ede7e1525e7963b26f5ad3eaec3b2
tim reid <timreid@gmail.com>
2018-09-27 12:35:59 +0200

    few fixes and updated readme

--- compiledGenome.py
+++ compiledGenome.py
@@ -6,7 +6,8 @@ def compileGenome(genome):
     def evalNode(i):
         #base case: input nodes simply output their value
         if i < genome['n']:
-            return f"i{i}"
+            return f"iN[{i}]"
+            # return f"iN[{i}]"
         else:
             #since input nodes are not present in the node list node i is actually at position i - n
             node = genome['nodes'][i - genome['n']]
@@ -18,14 +19,21 @@ def compileGenome(genome):
             paramaters = [evalNode(x) for x in node['connections']]
             #return the output of f applied to its paramaters
             signature = f"{fmodule + '.' + fname}({', '.join(paramaters)})"
+            # signature = f"{fmodule + '.' + fname}({paramaters})"
             #signature = f"{fname}({', '.join(paramaters)})"
 
             return signature
-    header = "f = lambda " + ', '.join(['i' + str(i) for i in range(genome['n'])]) + ":"
+    # header = "f = lambda " + ', '.join(['i' + str(i) for i in range(genome['n'])]) + ":"
+    header = "f = lambda iN:"
+
     body = f'({", ".join([evalNode(output) for output in genome["outputs"]])})'
+    #body = f'{[evalNode(output) for output in genome["outputs"]]}'
+   # body = f'{[evalNode(output) for output in genome["outputs"]]}'
+
     ff = header + body
     #env = {'operations':operations}
     env = {}
+    # print(ff)
     exec(ff, globals(), env)
     f = env['f']
     return f
@@ -40,7 +48,7 @@ def makeCompiledFitness(trainingData):
             #y is the dependant point from the range
             y = sample[1]
             #fx is the approximation of the dependant point from the genome
-            fx = genomeFunction(*x)
+            fx = genomeFunction(x)
             for s in range(len(y)):
                 #accumulate the absolute mean error
                 error += abs(y[s] - fx[s])
--- fitness.py
+++ fitness.py
@@ -1,7 +1,7 @@
 import random
 import cgp 
 
-def makeTrainingData(f, n, a, b, nSamples):
+def makeSImpleTrainingData(f, n, a, b, nSamples):
     samples = []
     for i in range(nSamples):
         x = [random.uniform(a,b) for j in range(n)]
@@ -9,17 +9,23 @@ def makeTrainingData(f, n, a, b, nSamples):
         samples.append((x,fx))
     return samples
 
-def makeSimpleFitness(trainingData):
+
+
+def makeSimpleFitness(labledData, compile):
     def fitness(genome):
         #calculate error on trainingData
         error = 0.0
-        for sample in trainingData:
+        for sample in labledData:
             #x is the independant point from the domain
             x = sample[0]
-            #y is the dependant point from the range
-            y = sample[1]
+            #label is the labled dependant point from the range
+            label = sample[1]
             #fx is the approximation of the dependant point from the genome
-            fx = cgp.evaluateGenome(genome, x)
+            if compile:
+                genomeFunction = compiledGenome.compileGenome(genome)
+                fx = genomeFunction(x)
+            else:
+                fx = cgp.evaluateGenome(genome, x)
             for s in range(len(y)):
                 #accumulate the absolute mean error
                 error += abs(y[s] - fx[s])
